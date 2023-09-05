221e19440a6f42dccdccafec8a4184b700b3650f
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-05 13:23:49 +0300

    first lesson, fundamentals things

---
+++ 01_C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Girin: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Girin: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + " " + surname); //birleştirmek için + kullandık.
+
+
+
+        }
+    }
+}
