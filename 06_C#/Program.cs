57d4b95b46a1957a0c1a9862514afe544e3e7f16
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-06 19:41:52 +0300

    If-Else and Ternary If

---
+++ 06_C#/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time <11)
+                Console.WriteLine("Günaydın!");
+            else if(time<=18)
+                Console.WriteLine("İyi günler!");
+            else
+                Console.WriteLine("İyi geceler!");
+            
+            //TEK SATIRDA DA OLUŞABİLİR 
+            string sonuc;
+            sonuc = time>=6 && time<11 ? "Günaydın!" : time<=18 ? "İyi günler!" : "İyi geceler!";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
