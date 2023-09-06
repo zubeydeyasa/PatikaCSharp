fa21a4cb5174c592f8d0640f0f8eb61b106c6a92
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-06 16:22:04 +0300

    operators

---
+++ 03_C#/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atama ve işlemli atama
+            int x = 3;
+            int y = 4;
+            y = y+2;
+
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y/=1;
+            Console.WriteLine(y);
+            x *=2;
+            Console.WriteLine(x);
+
+            //Mantıksal operatörler
+            bool issuc = true;
+            bool iscom = false;
+
+            if(issuc && iscom)
+                Console.WriteLine("Perfect");
+            if(issuc || iscom)
+                Console.WriteLine("Great");
+            if(issuc && !iscom)
+                Console.WriteLine("fine");
+
+            //İlişkisel operatörler
+            int a = 3;
+            int b = 4;
+            bool sonuc = a<b;
+
+            Console.WriteLine(sonuc);
+            sonuc = a>b;
+            Console.WriteLine(sonuc);
+            sonuc = a==b;
+            Console.WriteLine(sonuc);
+            sonuc = a!=b;
+            Console.WriteLine(sonuc);
+
+            //Aritmatik operatörler
+            int sayi1 = 2;
+            int sayi2 = 4;
+            int sonuc2 = sayi1 + sayi2;
+
+            Console.WriteLine(sonuc2);
+            sonuc2 = sayi1-sayi2;
+            Console.WriteLine(sonuc2);
+            sonuc2 = sayi1*sayi2;
+            Console.WriteLine(sonuc2);
+            sonuc2 = 20%3;
+            Console.WriteLine(sonuc2);
+        }
+    }
+}
