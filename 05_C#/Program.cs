89c45b3d67b05131de7e8dff012ea707bcd35d27
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-06 17:57:59 +0300

    logical errors and try,catch,finally

---
+++ 05_C#/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try //Hataya sebebiyet verme ihtimali olan kod 
+            {
+                Console.WriteLine("bir sayı giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.Write("girmiş olduğunuz sayı:" + sayi);
+            }
+            catch(Exception ex) //Hata ile karşılaşıldığında ne yapılacağı buraya yazılır
+            {
+                Console.WriteLine("Hata:" + ex.Message.ToString());
+            }
+            finally //Hata olsun olmasın mutlaka yapılmasını istediğimiz işler varsa buraya yazarız
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
