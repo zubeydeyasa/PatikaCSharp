4c87917569336d4e123ddb7c474d7c0ca94e58df
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-11 11:41:06 +0300

    While - Foreach

---
+++ 09_C#/Program.cs
@@ -0,0 +1,42 @@
+﻿﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //consoldan alınan sayıdan (sayı dahil) ortalama döndüren program
+            Console.WriteLine("lütfen bir sayı giriniz");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            // a dan z ye kadar tüm harfleri konsole yazdıran bir program
+            char a = 'a';
+            while(a<='z')
+            {
+                Console.Write(a);
+                a++;
+            }
+
+            //Foreach
+            Console.WriteLine("********Foreach**********");
+            string[] arabalar = {"BMV", "Nissan", "Toyota", "Ford"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
