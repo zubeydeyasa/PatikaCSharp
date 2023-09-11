ae682b1ca25f4dde65f9fdf16c342f6daf18d992
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-11 17:17:14 +0300

    Array.Clear-Array.Sort-Array.IndexOf-Array.Reverse

---
+++ 11_C#/Program.cs
@@ -0,0 +1,38 @@
+﻿﻿﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort
+            int[] sayiDizisi = {23,33,45,78,99,1,3,5,9};
+            Console.WriteLine("******Sırasız Dizi******");
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Console.WriteLine("******Sırasız Dizi******");
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //Clear
+            Console.WriteLine("******Array Clear******");
+            Array.Clear(sayiDizisi,2,2);//sayiDizisinin 2.indexten itibaren 2 tanesine 0 atar.
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            //Reverse
+            Console.WriteLine("******Array Reverse******");
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+            
+            //İndexof
+            Console.WriteLine("******Array IndexOf******");
+            Console.WriteLine(Array.IndexOf(sayiDizisi,99));
+        }
+    }
+}
