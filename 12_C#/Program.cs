37dbeeaeab2d92a55b15d62319cd594fab6d9e31
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-09-14 22:49:40 +0300

    Method definition && Ref

---
+++ 12_C#/Program.cs
@@ -0,0 +1,42 @@
+﻿﻿using System;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metodlar ornek = new Metodlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+        }
+        static int Topla(int deger1, int deger2)
+        {
+            return(deger1 + deger2);
+        }
+    }
+
+    class Metodlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public int ArttirVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1+=1;
+            deger2+=2;
+            return(deger1+deger2);
+        }
+    }
+}
