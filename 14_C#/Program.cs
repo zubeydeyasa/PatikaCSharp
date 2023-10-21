7792830a0bd5c7822d29177ff5a8eee701a28f70
zubeydeyasa <110638164+zubeydeyasa@users.noreply.github.com>
2023-10-21 22:44:28 +0300

    Recursive && Extension Method

---
+++ 14_C#/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace  recursive_extention_metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recursive 
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+                result = result * 3;
+            Console.WriteLine(result);
+
+            Works works = new();
+            Console.WriteLine(works.Expo(3,4));
+
+            //Extension metodlar
+            string ifade = "zubeyde yasa";
+            bool sonuc;
+            sonuc = ifade.CheckSpaces();
+            Console.WriteLine(sonuc);
+
+        }
+    }
+    public class Works
+    {
+        public int Expo(int sayi, int üs)
+        {
+            if(üs < 2)
+                return sayi;
+            return Expo(sayi, üs-1) * sayi;
+        }
+
+        public static class Extension
+        {
+            public static bool CheckSpaces(string param)
+            {
+                return param.Contains(" ");
+            }
+            
+        }
+    }
+    
+}
